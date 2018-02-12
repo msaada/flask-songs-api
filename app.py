35841f022f45002779336b744cdf281876a082d9
Mickaël Saada <mickael.saada@fluo.com>
2018-02-12 09:21:49 +0100

    refactor(app): mock all routes

--- app.py
+++ app.py
@@ -1,88 +1,33 @@
 import json
 import sqlite3
 from flask import Flask, request, jsonify
+from models import init_db
 
 
 app = Flask(__name__)
 
 
 @app.route('/api/songs', methods=['GET', 'POST'])
-def collection():
+def songs():
     if request.method == 'GET':
-        all_songs = get_all_songs()
-        return json.dumps(all_songs)
+        return not_implemented_yet_response()
     elif request.method == 'POST':
-        data = request.form
-        result = add_song(data['artist'], data['title'], data['rating'])
-        return jsonify(result)
+        return not_implemented_yet_response()
 
 
 @app.route('/api/song/<song_id>', methods=['GET', 'PUT', 'DELETE'])
-def resource(song_id):
+def song(song_id):
     if request.method == 'GET':
-        song = get_single_song(song_id)
-        return json.dumps(song)
+        return not_implemented_yet_response()
     elif request.method == 'PUT':
-        data = request.form
-        result = edit_song(
-            song_id, data['artist'], data['title'], data['rating'])
-        return jsonify(result)
+        return not_implemented_yet_response()
     elif request.method == 'DELETE':
-        result = delete_song(song_id)
-        return jsonify(result)
+        return not_implemented_yet_response()
 
-
-# helper functions
-
-def add_song(artist, title, rating):
-    try:
-        with sqlite3.connect('songs.db') as connection:
-            cursor = connection.cursor()
-            cursor.execute("""
-                INSERT INTO songs (artist, title, rating) values (?, ?, ?);
-                """, (artist, title, rating,))
-            result = {'status': 1, 'message': 'Song Added'}
-    except:
-        result = {'status': 0, 'message': 'error'}
-    return result
-
-
-def get_all_songs():
-    with sqlite3.connect('songs.db') as connection:
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM songs ORDER BY id desc")
-        all_songs = cursor.fetchall()
-        return all_songs
-
-
-def get_single_song(song_id):
-    with sqlite3.connect('songs.db') as connection:
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM songs WHERE id = ?", (song_id,))
-        song = cursor.fetchone()
-        return song
-
-
-def edit_song(song_id, artist, title, rating):
-    try:
-        with sqlite3.connect('songs.db') as connection:
-            connection.execute("UPDATE songs SET artist = ?, title = ?, rating = ? WHERE ID = ?;", (artist, title, rating, song_id,))
-            result = {'status': 1, 'message': 'SONG Edited'}
-    except:
-        result = {'status': 0, 'message': 'Error'}
-    return result
-
-
-def delete_song(song_id):
-    try:
-        with sqlite3.connect('songs.db') as connection:
-            connection.execute("DELETE FROM songs WHERE ID = ?;", (song_id,))
-            result = {'status': 1, 'message': 'SONG Deleted'}
-    except:
-        result = {'status': 0, 'message': 'Error'}
-    return result
-
+def not_implemented_yet_response():
+    return jsonify({'status': 'NOT_IMPLEMENTED_YET'})
 
 if __name__ == '__main__':
+    init_db()
     app.debug = True
     app.run()
